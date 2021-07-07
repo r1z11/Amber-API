53f2394856211889737c3bcc2bbc53c3bb485d86
Eric Sentongo <eric_sentongo@yahoo.com>
2021-07-08 00:23:10 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const db = require('./config/db');
 
 const app = express();
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://amber-s-api.herokuapp.com');
+    res.header('Access-Control-Allow-Origin', 'https://amber-s-api.herokuapp.com/*');
     next();
 });
 
