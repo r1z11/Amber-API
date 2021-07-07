8b14088b80e2cd947123feae88cc788d7150822c
Eric Sentongo <eric_sentongo@yahoo.com>
2021-07-08 00:20:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const port = process.env.PORT || 3001;
 
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.json());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://amber-s-api.herokuapp.com');
+    next();
+});
 
 const client = new MongoClient(db.url, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
